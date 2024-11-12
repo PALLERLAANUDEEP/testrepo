478deae16789b6254d52878b220a2e88592f16b2
PALLERLAANUDEEP <pallanu57@gmail.com>
2024-11-12 16:51:52 +0530

    added Test.java file

---
+++ Test.java
@@ -0,0 +1,13 @@
+package anudeep;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "hii";
+		System.out.println(s1);
+		
+
+	}
+
+}
